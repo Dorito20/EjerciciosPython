7bac0b9137fd0e46c6273d231f3239e1c7cc2bdc
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-07-16 20:26:46 -0500

    Agregar la matrix

---
+++ LaMatrix.py
@@ -0,0 +1,10 @@
+# Escribe un programa que solicite al usario
+# que ingrese su nombre. El nombre se debe almacenar
+# en una variable llamada nombre. 
+# A continuacion se debe mostrar en pantalla
+# el texto "ahora estas en matrix, [usuario]" donde
+# "[usuario]" se reemplaza por el nombre que
+# el usuario haya ingresado
+
+nombre = input("Escribe un nombre: ")
+print("Ahora estas en la matrix," + nombre)
