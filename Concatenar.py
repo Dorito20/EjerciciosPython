01caa66430a95ca0e92e9208f1e2a64b4efe6312
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-07-23 20:56:03 -0500

    te

---
+++ Concatenar.py
@@ -0,0 +1,9 @@
+# Escriba un progarma que solicite al usuario el ingreso de dos palabras,
+# las cuales se guardarán en dos variables distintas.
+# A continuacióm, almacene en un variable la contenación de la primera palabra, 
+# más un espacio, más la segunda palabra. muetre el resusltado en pantalla.
+
+p1 = input("ingresar la primera palabra: ")
+p2 = input("ingresar la segunda palabra: ")
+p3 = p1 +" "+ p2
+print(p3)
