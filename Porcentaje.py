751655890596b6df2fa2e7c00d8ac32e8f4b976a
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-07-23 20:09:37 -0500

    Numero solicitado

---
+++ Porcentaje.py
@@ -0,0 +1,12 @@
+# Escribe un porsentaje que solicita al usuario
+# un número y le resta el 15%, alamacenando
+# todo en una única variable. A continuación,
+# mostrar el resultado final en pantalla.
+
+N = int(input("Numero solicitado: "))
+
+R1 = N * 15
+R2 = R1/100
+Resta = N - R2
+
+print(Resta)
